1d4819171332718cb55026fdae1def52f24ff468
Nachiket Balame <nachiketbalame001@gmail.com>
2024-12-13 14:37:36 +0530

    Final

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,11 +2,11 @@ import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "15d",
+    expiresIn: "1d",
   });
 
   res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000, // Mili-Seconds
+    maxAge: 1 * 24 * 60 * 60 * 1000, // Mili-Seconds
     httpOnly: true,
     sameSite: "strict",
     secure: process.env.NODE_ENV !== "development",
